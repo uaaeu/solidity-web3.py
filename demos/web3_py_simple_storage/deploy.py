7d71e7fa986993a1529bde50d7446716c66b33e3
uaaeu <yurtmusa@gmail.com>
2022-01-04 15:35:22 +0300

    .env and gitignore

--- demos/web3_py_simple_storage/deploy.py
+++ demos/web3_py_simple_storage/deploy.py
@@ -1,12 +1,15 @@
 from solcx import compile_standard
-import json
 from web3 import Web3
+from dotenv import load_dotenv
+import json
+import os
+
+load_dotenv()
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
 
 # Compile Our Solidity
-
 compiled_sol = compile_standard(
     {
         "language": "Solidity",
@@ -33,10 +36,11 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # For Connecting to Ganache
-w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
-chain_id = 5777
-my_address = "0xc14fc1ECa33B18FEBF994B7847CdD615415BEB40"
-private_key = "0x635962a9c407f7efde903f7699f236d0fc9ed194ad80100a91181e14ce40c2e9"
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0xCeb65ce05Dd48b3ddDEd0Af9d16E3E26DF7d0fA2"
+private_key = os.getenv("PRIVATE_KEY")
+print(private_key)
 
 # Create the Contract in Python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
@@ -47,3 +51,12 @@ nonce = w3.eth.getTransactionCount(my_address)
 # 1. Build a Transaction
 # 2. Sign a Transaction
 # 3. Send a Transaction
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
